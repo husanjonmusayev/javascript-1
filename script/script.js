5ce6abbb0a5e63a2bf06f65be69f2743ca765c5d
husanjonmusayev <husanjonm279@gmail.com>
2023-11-27 08:55:21 +0500

    vazifa ishi

---
+++ script/script.js
@@ -0,0 +1,83 @@
+// birinchi masala
+/* let a = +prompt(' ')
+let b = +prompt(' ')
+
+if (a > b){
+    console.log(a**2);
+
+}else{
+console.log(b**2);
+} */
+
+// ikkinchi masala
+// let a =+prompt('brinchi son')
+// let b =+prompt("ikkinchi son")
+// let c =+prompt("uchinchi son")
+
+// if (a>b && a>c){
+//     console.log(a)
+// }if(b>a && b>c){
+//     console.log(b)
+// }if(c>a && c>b){
+//     console.log(c)
+// }
+
+// uchinchi masala
+// let n = +prompt(" nomalum son ")
+// if (n%2==0){
+//     console.log(n,"juft son");
+// }else{
+//     console.log(n,"toq son");
+// }   
+
+// to'tinchi masala
+// let n = +prompt("beshka karrali son kirgizing")
+// if(n%5==0){
+//  console.log(n,"beshka karrali");
+// }else{
+//     console.log(n,"beshka karrali emas");
+// }
+
+// beshinchi masala
+// let n =+prompt(" ")
+// console.log(n/10);
+
+// oltinchi masala
+// let n = +prompt('birlar honasidagi sonni topish')
+// console.log(n%10);
+
+// yettinchi masala
+// let n = +prompt('nomalum sonni kiriting')
+// let a= (n%10)
+// let b=(n/10)
+// if(a>b){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// sakkizinchi masala
+// let n = +prompt(' sonni kiriting ')
+// let a =(n%10)
+// if(a%2==0){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// to'qizinchi masala
+// let n = +prompt('sonni kirgazing')
+// console.log(n/100);
+
+// oninchi masala
+// let n = +prompt('sonni kriting')
+// let a = (n%10)
+// let b = ((n/10)%10)
+// let c = (n/100)
+// if(a>b && a>c){
+//     console.log(a);
+// }if(b>a && b>c){
+//     console.log(b);
+// }if(c>a && c>b){
+//     console.log(b);
+// }
